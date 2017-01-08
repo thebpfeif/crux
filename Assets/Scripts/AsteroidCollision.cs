4d4df31275d6cfe23fbb62dd535b1dc13c8f8d67
Brandon Pfeifer <thebpfeifer@gmail.com>
2017-01-08 17:20:16 -0600

    Added item dropping to asteroid game object

--- Assets/Scripts/AsteroidCollision.cs
+++ Assets/Scripts/AsteroidCollision.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class AsteroidCollision : MonoBehaviour {
 
+    public GameObject ItemDropGO; 
+
 	// Use this for initialization
 	void Start () {
 	
@@ -17,7 +19,18 @@ public class AsteroidCollision : MonoBehaviour {
     {
         if(collider.tag == "PlayerLaser")
         {
+            /*get position of asteroid before destroying*/
+            Vector2 asteroid_pos = transform.position;
+
+            /* Destroy asteroid */ 
             DestroyObject(gameObject);
+
+            /*Generate items for the player to pick up*/
+            GameObject item = (GameObject)Instantiate(ItemDropGO);
+
+            /* Set item drop location equal to asteroid's position*/ 
+            item.transform.position = asteroid_pos;
+
         }
     }
 }
