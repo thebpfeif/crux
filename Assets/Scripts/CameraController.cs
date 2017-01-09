432fa133166d9bd69fd90c241ca8e3f15352473a
Brandon Pfeifer <thebpfeifer@gmail.com>
2017-01-08 18:15:25 -0600

    Camera now follows player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset; 
+
+	// Use this for initialization
+	void Start ()
+    {
+        offset = transform.position - player.transform.position; 
+	}
+	
+	// LateUpdate is called once per frame but not until all other 
+    // items have been processed in Update() 
+	void LateUpdate ()
+    {
+        transform.position = player.transform.position + offset; 
+	}
+}
