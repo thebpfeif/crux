7fa228e02b24a5d40cd8b0e88a03c8958f562260
Brandon Pfeifer <thebpfeifer@gmail.com>
2017-07-17 23:23:09 -0500

    Items picked up by the ship are now tracked and a total is displayed on the screen

---
+++ Assets/Scripts/PlayerItems.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class PlayerItems : MonoBehaviour {
+
+    private float itemCount;
+    public Text countText; 
+
+	// Use this for initialization
+	void Start () {
+        itemCount = 0;
+        SetCountText(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        /* If we encounter an item, pick it up */
+        if (collider.tag == "ItemPickup")
+        {
+            itemCount++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Item Count: " + itemCount.ToString();
+    }
+}
